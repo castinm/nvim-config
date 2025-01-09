861d713ce0fa78907abaa0df2b1da9f13f577f48
Castin Masika <masikacastin@gmail.com>
2025-01-09 12:13:53 +0300

    restore default terminal cursor on exiting neovim

--- init.lua
+++ init.lua
@@ -5,3 +5,9 @@ require("auto-complete")
 require("nvim-tree-config")
 require("mappings")
 require("theme")
+
+
+-- 
+-- Restore terminal cursor on exiting
+vim.cmd([[autocmd VimLeave * set guicursor= | call chansend(v:stderr, "\x1b[ q")]])
+
--- lua/theme.lua
+++ lua/theme.lua
@@ -1,5 +1,6 @@
 require("barbar-config")
 
+vim.opt.autoindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
@@ -7,3 +8,4 @@ vim.bo.softtabstop = 2
 vim.wo.relativenumber = true
 
 vim.cmd("colorscheme kanagawa")
+vim.cmd("syntax on")
