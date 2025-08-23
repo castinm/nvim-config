a19c637b4e5e05a3636ca47cd8c1e3decfa3a48c
Castin Masika <castinmasikah@protonmail.com>
2025-08-23 16:19:13 +0300

    force rust tab spaces to 2

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,11 +1,13 @@
 require("barbar-config")
 
+vim.cmd("colorscheme kanagawa")
+vim.cmd("syntax on")
+
 vim.opt.autoindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
 vim.wo.relativenumber = true
+vim.g.rust_recommended_style = 0
 
-vim.cmd("colorscheme kanagawa")
-vim.cmd("syntax on")
